e08b1e4aed1bd9087d45c9590cc8111babee08bb
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-01-28 08:32:56 -0500

    🚀 Feat: add SearchBar Component logic

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import STRINGS from "../constants/strings";
 
-const SearchBar = () => (
-  <div>
-    <input placeholder={STRINGS.HEADER.SEARCH_BAR_PLACEHOLDER} />
-    <button type="button">
-      <img src="" alt="" />
-    </button>
-  </div>
-);
+const SearchBar = () => {
+  const [query, setQuery] = useState();
+  let history = useHistory();
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    if (e && e.preventDefault) e.preventDefault();
+    const urlFormatQuery = encodeURIComponent(query);
+    history.push(`?q=${urlFormatQuery}`);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        placeholder={STRINGS.HEADER.SEARCH_BAR_PLACEHOLDER}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleSearch} type="button">
+        <img src="" alt="" />
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
