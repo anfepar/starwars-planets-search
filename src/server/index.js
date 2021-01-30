943b046a96e3188b759ad574fcf5465d0cc44fb2
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-01-30 11:30:55 -0500

    🔧 Fix: support assets in server side mode

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,12 @@
-require('ignore-styles');
+require("ignore-styles");
 
-require('@babel/register')({
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+require("@babel/register")({
+  presets: ["@babel/preset-env", "@babel/preset-react"],
 });
 
-require('./server');
+require("asset-require-hook")({
+  extensions: ["jpg", "png", "gif"],
+  name: "/assets/[hash].[ext]",
+});
+
+require("./server");
