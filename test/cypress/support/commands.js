4a63725e783a95f14a9a8fd2f5ec4c608c4013a0
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-02-01 01:24:22 -0500

    🧪 Test: add E2E tests to filter feature

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -1,9 +1,23 @@
-
 Cypress.Commands.add("searchPlanet", (planetName) => {
-    cy.visit("/");
-    cy.get(".searchBar__input").type(planetName)
-    cy.get(".searchBar__button").click();
-    cy.wait(1000);
-    cy.get(".TableItem").should("exist");
+  cy.visit("/");
+  cy.get(".searchBar__input").type(planetName);
+  cy.get(".searchBar__button").click();
+  cy.wait(1000);
+  cy.get(".TableItem").should("exist");
+});
+
+Cypress.Commands.add("filterPlanetsUsingSelect", (key, value) => {
+  cy.get(`#${key}`).select(value);
+  cy.get("tbody").contains(value).as("filteredElement");
+  cy.get("@filteredElement").then((filteredElement) => {
+    expect(filteredElement).to.exist;
   });
-  
+});
+
+Cypress.Commands.add("filterPlanetsUsingInput", (key, value) => {
+  cy.get(`#${key}`).type(value);
+  cy.get("tbody").contains(value).as("filteredElement");
+  cy.get("@filteredElement").then((filteredElement) => {
+    expect(filteredElement).to.exist;
+  });
+});
