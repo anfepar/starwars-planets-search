8e142783a9a97b283f98cd2b66cc4a33cc738eb5
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-02-01 17:18:44 -0500

    💅 Style: add styles to buttons in table header

--- src/frontend/components/TableHeadField.jsx
+++ src/frontend/components/TableHeadField.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setOrder } from "../actions";
-
 import "../assets/styles/components/TableHeadField.styl";
 import ORDER_TYPES from "../constants/orderTypes";
+import defaultArrow from "../assets/static/default-arrow.png";
+import upArrow from "../assets/static/up-arrow.png";
+import downArrow from "../assets/static/down-arrow.png";
 
 const TableHeadField = ({ name, value }) => {
   const dispatch = useDispatch();
   const { order } = useSelector((state) => state);
   const orderStates = [
-    { type: ORDER_TYPES.ASC, icon: "⬆️" },
-    { type: ORDER_TYPES.DESC, icon: "⬇️" },
+    { type: ORDER_TYPES.ASC, icon: upArrow },
+    { type: ORDER_TYPES.DESC, icon: downArrow },
   ];
   const [orderState, setOrderStatus] = useState(false);
 
@@ -32,12 +34,15 @@ const TableHeadField = ({ name, value }) => {
     const orderStateObj = orderStates.find(
       (entry) => entry.type === orderState
     );
-    return orderStateObj ? orderStateObj.icon : "";
+    return orderStateObj ? orderStateObj.icon : defaultArrow;
   };
 
   return (
-    <th onClick={toogleChangeOrder} className="TableHeadField">
-      {orderState ? `${name} ${getOrderIcon()}` : name}
+    <th className="TableHeadField" onClick={toogleChangeOrder}>
+      <div className="TableHeadField__content">
+        <p>{name}</p>
+        <img src={getOrderIcon()} alt="arrow" />
+      </div>
     </th>
   );
 };
