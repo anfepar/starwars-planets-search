c052056039a7dfed1227fec16bcdd5d76111c685
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-02-01 07:50:23 -0500

    🚀 Feat: change TabbleHeadField to be clickeable

--- src/frontend/components/TableHeadField.jsx
+++ src/frontend/components/TableHeadField.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/components/TableHeadField.styl";
 
-const TableHeadField = ({ name }) => <th className="TableHeadField">{name}</th>;
+const TableHeadField = ({ name }) => {
+  const orderStates = ["asc", "desc"];
+  const [order, setOrder] = useState(false);
+  const toogleChangeOrder = () => {
+    const stateIndex = orderStates.indexOf(order);
+    if (stateIndex + 1 > 1) setOrder(false);
+    setOrder(orderStates[stateIndex + 1]);
+  };
+
+  return (
+    <th onClick={toogleChangeOrder} className="TableHeadField">
+      {order ? (order === orderStates[0] ? `${name} ⬆️` : `${name} ⬇️`) : name}
+    </th>
+  );
+};
 export default TableHeadField;
